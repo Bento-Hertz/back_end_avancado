3b8883ca71558f95955f220cee88f2c659de3064
Bento-Hertz <100121560+Bento-Hertz@users.noreply.github.com>
2022-11-07 18:04:49 -0500

    alteracao

--- first-week/básico/js/index.js
+++ first-week/básico/js/index.js
@@ -150,33 +150,64 @@ console.log(dogJson)
 const dogObject = JSON.parse(dogJson)
 console.log(dogObject)
 
-const jsonErrado = '{"primeiro":"dado1""segundo":222,"terceiro":"dado3}'
-const jsonErradoObject = JSON.parse(jsonErrado)
+//const jsonErrado = '{"primeiro":"dado1""segundo":222,"terceiro":"dado3}'
+//const jsonErradoObject = JSON.parse(jsonErrado)
 
-/*Desafio 1: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto 
-com as propriedades n1, n2, n3, n4 e n5. Utilize destructuring para copiar os valores 
-para 5 variáveis. Construa um objeto JSON com os valores das variáveis.*/
+// Desafio 1: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto com as propriedades n1, n2, n3, n4 e n5. Utilize destructuring para copiar os valores para 5 variáveis. Construa um objeto JSON com os valores das variáveis.
 
-const vectorChallenge = [77, 45, 3, 8, 123]
+const vectorChallenge = [77,45,3,8,123]
 
-const ObjectChallenge = {
+const objectChallenge = {
     n1: vectorChallenge[0],
     n2: vectorChallenge[1],
     n3: vectorChallenge[2],
     n4: vectorChallenge[3],
-    n5: vectorChallenge[4]
+    n5: vectorChallenge[4],
 }
 
-console.log("")
+const {n1, n2, n3, n4, n5} = objectChallenge
 
-//
+console.log(JSON.stringify({n1,n2,n3,n4,n5}))
+
+// Desafio 2: Desenvolva uma calculadora que armazene em um objeto JSON o resultado das 4 operações básicas. Em outras palavras: crie um objeto calculadora, inicialize cada uma das operações da calculadora (que serão propriedades do objeto) com o resultado do processamento matemático vindo de duas variáveis e converta para um objeto JSON.
 
 const num1 = 10
 const num2 = 20
 const calc = {
-    
+    soma: num1+num2,
+    sub: num1-num2,
+    mult: num1*num2,
+    div: num1/num2
 }
+console.log(JSON.stringify(calc))
 
 
 
+// estruturas, métodos de listas, funções
+const n = 100
+if(n > 10){ // > < >= <= == !=
+    console.log('Teste de entrada no if.')
+}
+const texto = '0'
+if(texto === 0)
+    console.log("Os dados são iguais.")
+else if(true) 
+    console.log('Segundo if - if aninhado.')
+else
+    console.log('Saída...') // erro na hora de pensar o código...
+// operador ternário
+const resultado =  n > 20 ? true : false
+console.log(resultado)
+console.log(typeof resultado)
+// repetição: precisa de 3 partes - variável de controle e a inicialização dela; condição/critério de parada; alteração do valor da variável de controle
+let contador = 0
+const lista = [1, 7, 3, 64, 2, 0]
+while(contador < lista.length){
+    console.log('O \'elemento\' da\n\n vez é ' + lista[contador] + '.')
+    contador++ // contador = contador + 1
+}
+const outraLista = ['a', 'b', 'd', 'e', 'c']
+for(let contador = 0; contador < outraLista.length; contador++)
+    console.log(`O elemento da
 
+    vez é ${outraLista[contador]}.`) //template literais, ou template strings
